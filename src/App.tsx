a328ff041667521c108bf8a11aca9fcc753748da
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-07-05 13:17:47 +0600

    Increment and Decrement button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 
 function App() {
@@ -7,8 +8,10 @@ function App() {
    <>
    <div>
     <h1 className="text-4xl mt-2 font-bold text-center">Counter</h1>
-    <div>
-      
+    <h3 className="text-center font-bold text-red-400 mt-6 mb-3">0</h3>
+    <div className="flex justify-center items-center gap-2 ">
+      <Button>Increment</Button>
+      <Button>Decrement</Button>
     </div>
    </div>
    </>
